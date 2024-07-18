ca69c3d257f50fd6bf128c05a56ca5f745ca9db8
ihoek <134209720+ihoek@users.noreply.github.com>
2024-07-18 09:36:55 +0900

    [level 0] Title: 나이 출력, Time: 0.03 ms, Memory: 4.12 MB -BaekjoonHub

---
+++ 프로그래머스/0/120820. 나이 출력/나이 출력.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int solution(int age) {
+    int answer = 0;
+    
+    cin >> age;
+    
+    answer=2022-age+1;
+    
+    return answer;
+}

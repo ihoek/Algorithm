c1de0bdab32d7dd707533b9f01bc233eaf49e9bd
ihoek <134209720+ihoek@users.noreply.github.com>
2025-01-18 17:42:15 +0900

    [level 0] Title: A 강조하기, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.js
@@ -0,0 +1,14 @@
+function solution(myString) {
+    var answer = '';
+    
+    for(let i=0;i<myString.length;i++){
+        if(myString[i] === 'a' ){
+            answer += myString[i].toUpperCase();
+        }else if(myString[i]  === "A"){
+            answer += myString[i];
+        }else{
+            answer += myString[i].toLowerCase();
+        }
+    }
+    return answer;
+}

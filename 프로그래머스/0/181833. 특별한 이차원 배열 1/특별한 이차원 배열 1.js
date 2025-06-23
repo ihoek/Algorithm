8e56c112be9f639367f24fc8041283a91c1ca92d
ihoek <134209720+ihoek@users.noreply.github.com>
2025-06-23 11:14:32 +0900

    [level 0] Title: 특별한 이차원 배열 1, Time: 0.23 ms, Memory: 38.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = [[]];
+    
+    answer = Array.from({ length: n }, () => Array(n).fill(0));
+    
+    for (let i = 0; i < n; i++) {
+        answer[i][i] = 1;
+    }
+    
+    return answer;
+}
